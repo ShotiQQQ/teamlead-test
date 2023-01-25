6acdc4c6d6d536919634730cafbe1612606c1e19
Denis <shotiqqq@gmail.com>
2023-01-25 12:41:27 +0700

    add timer

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,21 @@ const form = document.querySelector('#form-order');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 })
+
+const timer = document.querySelector('#timer');
+
+let countSec = 60;
+let countMin = 30;
+
+const timeout = setInterval(() => {
+  countSec -= 1;
+  timer.textContent = `${countMin} мин.`
+  if (countSec === 1) {
+    countMin -= 1;
+    countSec = 60;
+    if (countMin === 0) {
+      timer.textContent = `${countMin} мин.`
+      clearTimeout(timeout);
+    }
+  }
+}, 1000);
